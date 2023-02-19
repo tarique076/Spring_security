5ad7294ef4245445b53505e20db096bc02f441c1
Tarique <anwartarique408@gmail.com>
2023-02-19 15:49:56 +0530

    Service Implementations

---
+++ WithoutJWT/src/main/java/com/masai/services/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.masai.services;
+
+import java.util.List;
+
+import com.masai.entity.Customer;
+import com.masai.exception.CustomerException;
+
+public class CustomerServiceImpl implements CustomerService{
+
+	@Override
+	public Customer registerCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer getCustomerDetailsByEmail(String email) throws CustomerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getAllCustomerDetails() throws CustomerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
