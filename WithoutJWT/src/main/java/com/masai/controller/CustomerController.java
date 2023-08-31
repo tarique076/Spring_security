830c33e18184cdb5bbc16ddffda3be2fe8afbff8
Tarique <anwartarique408@gmail.com>
2023-08-31 13:15:04 +0530

    api endpoint updated

--- WithoutJWT/src/main/java/com/masai/controller/CustomerController.java
+++ WithoutJWT/src/main/java/com/masai/controller/CustomerController.java
@@ -15,7 +15,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerService cService;
 	
-	@PostMapping("/customers")
+	@PostMapping("/customer")
 	public ResponseEntity<Customer> registerCustomer(Customer cust){
 		
 		Customer regCustomer = cService.registerCustomer(cust);
