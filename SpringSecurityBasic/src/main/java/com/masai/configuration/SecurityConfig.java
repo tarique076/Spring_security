9fd2406a7a098ce7832ec0be4b5a91d7761bbfe6
Tarique <anwartarique408@gmail.com>
2023-09-14 15:13:23 +0530

    InMemoryUser implemented..

--- SpringSecurityBasic/src/main/java/com/masai/configuration/SecurityConfig.java
+++ SpringSecurityBasic/src/main/java/com/masai/configuration/SecurityConfig.java
@@ -3,6 +3,11 @@ package com.masai.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +20,25 @@ public class SecurityConfig {
 				.requestMatchers("/welcome/secured").authenticated()).httpBasic();
 		
 		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetails() {
+		
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		
+		UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+		UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+		
+		userDetailsService.createUser(admin);
+		userDetailsService.createUser(user);
+		
+		return userDetailsService;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
